ac758f40e6cc91db412edcc14a4780b664dc1198
Huntrt <huntrt.business@outlook.com>
2023-07-08 06:54:57 +0700

    Basic movement using preset direction

---
+++ 2019 Game/Assets/Scripts/Movement/Movement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+	public Rigidbody2D rb;
+    public float speed;
+	[SerializeField] protected Vector2 moveDir;
+
+	void FixedUpdate()
+	{
+		rb.MovePosition(rb.position + (moveDir.normalized * speed) * Time.fixedDeltaTime);
+	}
+}
