ee15d8ee53962ef2d29a17e2817b779c0957b34a
Huntrt <huntrt.business@outlook.com>
2023-07-08 11:37:57 +0700

    Function to die instantly after cast

---
+++ 2019 Game/Assets/Scripts/Combats/DieAfterCast.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class DieAfterCast : MonoBehaviour
+{
+    [SerializeField] Caster caster;
+	[SerializeField] Health health;
+
+	void OnEnable()
+	{
+		caster.onCast += CastDie;
+	}
+
+	void CastDie()
+	{
+		health.Die();
+	}
+
+	void OnDisable()
+	{
+		caster.onCast -= CastDie;
+	}
+}
