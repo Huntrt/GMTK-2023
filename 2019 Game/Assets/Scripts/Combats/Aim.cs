86fd875d9218f6616e86c1ca8b336055c7e74397
Huntrt <huntrt.business@outlook.com>
2023-07-08 09:27:21 +0700

    Aim system that handle attack target when detect

---
+++ 2019 Game/Assets/Scripts/Combats/Aim.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Aim : MonoBehaviour
+{
+	[SerializeField] Transform[] firepoints;
+	[SerializeField] Caster caster;
+	[SerializeField] float detect;
+    [SerializeField] Transform target; public Transform Target {get; set;}
+
+	void Update()
+	{
+		//Stop if there no target
+		if(target == null) return;
+		//Make all firepoint aim toward target
+		foreach (var firepoint in firepoints) firepoint.right = target.position - transform.position;
+		//Begin auto cast when the target get in detection
+		caster.autoCast = Vector2.Distance(transform.position, target.position) <= detect;
+	}
+}
---
+++ 2019 Game/Assets/Scripts/Combats/Aim_Protag.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Aim_Protag : Aim
+{
+    void Start()
+	{
+		Target = Protag.i.transform;
+	}
+}
