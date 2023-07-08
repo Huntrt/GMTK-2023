430de12d370a9f7cc05bbb3c9c045fd8fa4d4099
Huntrt <huntrt.business@outlook.com>
2023-07-09 01:43:18 +0700

    Formation of enemy to spawn multiple of them

---
+++ 2019 Game/Assets/Scripts/Spawning/Formation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Formation : MonoBehaviour
+{
+    [System.Serializable] class Form
+	{
+		public Transform point;
+		public GameObject enemy;
+	}
+	[SerializeField] Form[] forms;
+
+	void Start()
+	{
+		foreach (Form form in forms)
+		{
+			Pooler.i.Create(form.enemy, form.point.position, Quaternion.identity);
+		}
+		Destroy(gameObject);
+	}
+}
