a1fc8db053d380cb9c27be948a31d5839b468e61
Huntrt <huntrt.business@outlook.com>
2023-07-08 09:31:23 +0700

    Caster now have firerate and auto fire when needed

--- 2019 Game/Assets/Scripts/Combats/Caster.cs
+++ 2019 Game/Assets/Scripts/Combats/Caster.cs
@@ -2,5 +2,26 @@
 
 public class Caster : MonoBehaviour
 {
-    
+	public bool autoCast, isReady;
+	public float firerate; float firerateTimer;
+
+	void Update()
+	{
+		//When has reached the timer for firerate
+		if(firerateTimer >= 1/firerate)
+		{
+			//Is now ready
+			isReady = true;
+			//Instantly cast if autoing
+			if(autoCast) Cast();
+		}
+		//Timing firerate when not ready
+		if(!isReady) firerateTimer += Time.deltaTime;
+	}
+
+    public virtual void Cast()
+	{
+		//Don't cast if not ready
+		if(!isReady) return;
+	}
 }
