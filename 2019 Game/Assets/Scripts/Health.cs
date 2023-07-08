57bf275eac22b7a29791ffb6a6d1bbd8c0832652
Huntrt <huntrt.business@outlook.com>
2023-07-08 09:23:09 +0700

    Added basic health system

---
+++ 2019 Game/Assets/Scripts/Health.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] float maxHealth, curHealth;
+	public float MaxHealth {get => maxHealth; set{maxHealth = value; Damaging(0);}}
+	public float CurHealth {get => curHealth;}
+	public delegate void onDamage(float taken); public onDamage OnDamage;
+	public delegate void onDeath(); public onDeath OnDeath;
+
+	void OnEnable()
+	{
+		//Use max health as current health upon spawn
+		curHealth = maxHealth;
+	}
+
+	public void Damaging(float taken)
+	{
+		//Current health got decrease by damage taken
+		curHealth -= taken;
+		//Prevent health from going out of bounds
+		curHealth = Mathf.Clamp(curHealth,0,maxHealth);
+		//Send taken damage event
+		OnDamage?.Invoke(taken);
+		//Got hurt when take positive damage
+		if(taken > 0) Hurted(taken);
+		//Get heal when take negative damage
+		if(taken < 0) Healed(taken);
+		//Die when run out of health and no longer consider hurt
+		if(curHealth <= 0) {Die(); return;}
+	}
+
+	void Hurted(float damaged)
+	{
+		//... Do something after hurted
+	}
+
+	void Healed(float recovered)
+	{
+		//... Do something after got heal
+	}
+
+	public void Die()
+	{
+		//Send death event
+		OnDeath?.Invoke();
+		//Deactive the object upon death
+		gameObject.SetActive(false);
+	}
+}
