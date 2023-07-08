bca382954bf88a31936ccf371b56238537c2d19b
Huntrt <huntrt.business@outlook.com>
2023-07-08 09:32:16 +0700

    Caster that will create projectile for each transform

---
+++ 2019 Game/Assets/Scripts/Combats/Caster_Points.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Caster_Points : Caster
+{
+	[SerializeField] GameObject projectile;
+    [SerializeField] Transform[] points;
+
+	public override void Cast()
+	{
+		base.Cast();
+		//Go through all the point
+		foreach (Transform point in points)
+		{
+			//Pool projectile at this point position and using point rotation
+			Pooler.i.Create(projectile, point.position, point.rotation);
+		}
+		//No longer ready
+		isReady = false;
+	}
+}
