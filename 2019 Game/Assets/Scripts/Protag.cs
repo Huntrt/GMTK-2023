0e46ee37de6b24b149c47d98d416140094456847
Huntrt <huntrt.business@outlook.com>
2023-07-08 07:08:20 +0700

    Main script for the 'protagonist'

---
+++ 2019 Game/Assets/Scripts/Protag.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Protag : MonoBehaviour
+{
+    #region Set this class to singleton
+	static Protag _i; public static Protag i
+	{
+		get
+		{
+			if(_i==null)
+			{
+				_i = GameObject.FindObjectOfType<Protag>();
+			}
+			return _i;
+		}
+	}
+	#endregion
+
+	
+}
