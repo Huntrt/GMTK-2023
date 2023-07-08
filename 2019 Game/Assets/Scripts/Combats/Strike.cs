c90b8ec3233131eeca709ed7e7486477bc2e837c
Huntrt <huntrt.business@outlook.com>
2023-07-08 09:26:40 +0700

    Added basic info to strike

--- 2019 Game/Assets/Scripts/Combats/Strike.cs
+++ 2019 Game/Assets/Scripts/Combats/Strike.cs
@@ -1,6 +1,12 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System;
 
 public class Strike : MonoBehaviour
 {
-    
+	public LayerMask damageLayer;
+	public Collider hurtbox;
+    public float damage, velocity, range;
+	public int piercing; protected List<Collider> pierceds = new List<Collider>();
+	public delegate void onContact(Health contacted, Vector2 contactPos); public onContact OnContact;
 }
