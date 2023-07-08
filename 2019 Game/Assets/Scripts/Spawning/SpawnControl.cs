a63884436cf6934173f951d000727f1aa2113383
Huntrt <huntrt.business@outlook.com>
2023-07-08 21:01:42 +0700

    List of slot that could be select when press key

--- 2019 Game/Assets/Scripts/Spawning/SpawnControl.cs
+++ 2019 Game/Assets/Scripts/Spawning/SpawnControl.cs
@@ -1,19 +1,41 @@
-﻿using UnityEngine;
-using TMPro;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class SpawnControl : MonoBehaviour
 {
-	#region Set this class to singleton
-	static SpawnControl _i; public static SpawnControl i
+	[SerializeField] List<SpawnSlot> slots = new List<SpawnSlot>();
+	[SerializeField] int currentlySelect;
+	SpawnSlot selected {get => slots[currentlySelect];}
+
+	void Start()
 	{
-		get
-		{
-			if(_i==null)
-			{
-				_i = GameObject.FindObjectOfType<SpawnControl>();
-			}
-			return _i;
-		}
+		Selecting(0);
 	}
-	#endregion
+
+	void Update()
+	{
+		#region Select with key
+		//todo: replace with keybinds (clean up?)
+		if(Input.GetKeyDown(KeyCode.Alpha1)) Selecting(0);
+		if(Input.GetKeyDown(KeyCode.Alpha2)) Selecting(1);
+		if(Input.GetKeyDown(KeyCode.Alpha3)) Selecting(2);
+		if(Input.GetKeyDown(KeyCode.Alpha4)) Selecting(3);
+		if(Input.GetKeyDown(KeyCode.Alpha5)) Selecting(4);
+		if(Input.GetKeyDown(KeyCode.Alpha6)) Selecting(5);
+		if(Input.GetKeyDown(KeyCode.Alpha7)) Selecting(6);
+		if(Input.GetKeyDown(KeyCode.Alpha8)) Selecting(7);
+		if(Input.GetKeyDown(KeyCode.Alpha9)) Selecting(8);
+		if(Input.GetKeyDown(KeyCode.Alpha0)) Selecting(9);
+		#endregion
+	}
+	void Selecting(int index)
+	{
+		//Turn the previous select slot indicator to white
+		selected.selectIndicator.color = Color.white;
+		//Now select at given index
+		currentlySelect = index;
+		//Currently select slot indicator to be yellow
+		selected.selectIndicator.color = Color.yellow;
+	}
+	}
 }
