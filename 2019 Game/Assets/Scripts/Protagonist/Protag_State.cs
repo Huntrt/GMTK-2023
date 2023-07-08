f74fe1a38236b928bc21b2cc8db743b21b9d32ac
Huntrt <huntrt.business@outlook.com>
2023-07-09 03:27:59 +0700

    Manage the protagonist state (hold off for now)

---
+++ 2019 Game/Assets/Scripts/Protagonist/Protag_State.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Protag_State : MonoBehaviour
+{
+    [SerializeField] enum State
+	{
+		Idle,// do nothing mostly switch to stomp instantly.
+		CrowdControl, // use fast firirng weapon when weak enemy come near.
+		Vengeance, //attack enemy that target it from far away.
+		Stomping, // when no threat begin stomp he floor.
+		Reviving, // 10 second of unable to do anything.
+	}
+	[SerializeField] State currentState;
+
+	void Update()
+	{
+		
+	}
+}
