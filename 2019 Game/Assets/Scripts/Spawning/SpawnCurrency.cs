39292dfde0b121d8857cab0c20f63632dc29e406
Huntrt <huntrt.business@outlook.com>
2023-07-08 17:57:18 +0700

    Basic currency manager

--- 2019 Game/Assets/Scripts/Spawning/SpawnCurrency.cs
+++ 2019 Game/Assets/Scripts/Spawning/SpawnCurrency.cs
@@ -3,5 +3,31 @@ using TMPro;
 
 public class SpawnCurrency : MonoBehaviour
 {
-    
+    [SerializeField] int cash; public int Cash {get => cash;}
+	[Header("GUI")]
+	[SerializeField] TextMeshProUGUI cashDisplay;
+
+	public void Earning(int earned)
+	{
+		cash += earned;
+		RefreshCashDisplay();
+	}
+
+	public bool Spending(int spended)
+	{
+		//If have enough cast to spend
+		if(spended <= cash)
+		{
+			cash -= spended;
+			RefreshCashDisplay();
+			return true;
+		}
+		//Don't have enough
+		return false;
+	}
+
+	void RefreshCashDisplay()
+	{
+		cashDisplay.text = "$" + cash;
+	}
 }
