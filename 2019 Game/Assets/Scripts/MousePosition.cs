e0be6188fdb95e7ffafa47765f6b78cf98e7f352
Huntrt <huntrt.business@outlook.com>
2023-07-08 20:05:04 +0700

    Class for getting mouse position

---
+++ 2019 Game/Assets/Scripts/MousePosition.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class MousePosition : MonoBehaviour
+{
+    #region Set this class to singleton
+	static MousePosition _i; public static MousePosition i
+	{
+		get
+		{
+			if(_i==null)
+			{
+				_i = GameObject.FindObjectOfType<MousePosition>();
+			}
+			return _i;
+		}
+	}
+	#endregion
+
+	Camera cam;
+
+	void Awake()
+	{
+		cam = Camera.main;
+	}
+
+	public Vector2 World()
+	{
+		return cam.ScreenToWorldPoint(Input.mousePosition);
+	}
+
+	public Vector2 Screen()
+	{
+		return Input.mousePosition;
+	}
+}
