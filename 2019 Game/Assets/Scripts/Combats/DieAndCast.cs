388c38a5473f768d17ea77af8d25719b7f489936
Huntrt <huntrt.business@outlook.com>
2023-07-08 14:45:50 +0700

    Able to either cast when die or die when cast

---
+++ 2019 Game/Assets/Scripts/Combats/DieAndCast.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class DieAndCast : MonoBehaviour
+{
+    [SerializeField] Caster caster;
+	[SerializeField] Health health;
+	[SerializeField] bool castUponDie, dieUponCast;
+
+	void OnEnable()
+	{
+		caster.onCast += Die;
+		health.OnDeath += Cast;
+	}
+
+	void Die() {health.Die();}
+	void Cast() {caster.Casting(true);}
+
+	void OnDisable()
+	{
+		caster.onCast -= Die;
+		health.OnDeath -= Cast;
+	}
+}
