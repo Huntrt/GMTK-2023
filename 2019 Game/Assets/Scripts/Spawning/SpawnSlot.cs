7687b8b2adbe8fbde1d932a2e3249559defd0922
Huntrt <huntrt.business@outlook.com>
2023-07-08 19:22:47 +0700

    Handle display and storing spawn info

---
+++ 2019 Game/Assets/Scripts/Spawning/SpawnSlot.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
+public class SpawnSlot : MonoBehaviour
+{
+	public EnemySpawning spawning;
+	public Image iconImage;
+	public TextMeshProUGUI costText;
+}
