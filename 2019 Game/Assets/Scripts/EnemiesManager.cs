58d07ca0e5b366549f171d92e66a291e0a3eb0ae
Huntrt <huntrt.business@outlook.com>
2023-07-09 15:07:35 +0700

    Manage which enemy in scene

---
+++ 2019 Game/Assets/Scripts/EnemiesManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesManager : MonoBehaviour
+{
+    #region Set this class to singleton
+	static EnemiesManager _i; public static EnemiesManager i
+	{
+		get
+		{
+			if(_i==null)
+			{
+				_i = GameObject.FindObjectOfType<EnemiesManager>();
+			}
+			return _i;
+		}
+	}
+	#endregion
+
+	public List<Transform> enemies = new List<Transform>();
+}
--- 2019 Game/Assets/Scripts/Spawning/EnemySpawning.cs
+++ 2019 Game/Assets/Scripts/Spawning/EnemySpawning.cs
@@ -5,4 +5,14 @@ public class EnemySpawning : MonoBehaviour
     public int cost;
 	public float cooldown;
 	public Sprite icon;
+
+	void OnEnable()
+	{
+		EnemiesManager.i.enemies.Add(transform);
+	}
+
+	void OnDisable()
+	{
+		EnemiesManager.i.enemies.Remove(transform);
+	}
 }
