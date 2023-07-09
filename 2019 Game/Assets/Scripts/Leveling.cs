4d3edac0d23bbfc070a04578b086d64609bcaac8
Huntrt <huntrt.business@outlook.com>
2023-07-09 19:52:58 +0700

    Gain exp and level up when enemy die

---
+++ 2019 Game/Assets/Scripts/Leveling.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class Leveling : MonoBehaviour
+{
+	#region Set this class to singleton
+	static Leveling _i; public static Leveling i
+	{
+		get
+		{
+			if(_i==null)
+			{
+				_i = GameObject.FindObjectOfType<Leveling>();
+			}
+			return _i;
+		}
+	}
+	#endregion
+
+	[SerializeField] int level;
+    public float curExp, expNeeded;
+	[SerializeField] Image expBar;
+	[SerializeField] TMPro.TextMeshProUGUI expCount;
+
+	void Start()
+	{
+		RefreshGUI();
+	}
+
+	public void GainExp(float amount)
+	{
+		curExp += amount;
+		//If reached exp need for next level
+		if(curExp >= expNeeded)
+		{
+			//Reset to the exp leftover after level up
+			curExp = curExp - expNeeded;
+			//Level up
+			level++;
+			//?... do something when level up
+		}
+		RefreshGUI();
+	}
+
+	void RefreshGUI()
+	{
+		expBar.fillAmount = curExp/expNeeded;
+		expCount.text = curExp + "/" + expNeeded;
+	}
+}
--- 2019 Game/Assets/Scripts/Spawning/EnemySpawning.cs
+++ 2019 Game/Assets/Scripts/Spawning/EnemySpawning.cs
@@ -4,6 +4,7 @@ public class EnemySpawning : MonoBehaviour
 {
     public int cost;
 	public float cooldown;
+	public float exp;
 	public Sprite icon;
 
 	void OnEnable()
@@ -13,6 +14,7 @@ public class EnemySpawning : MonoBehaviour
 
 	void OnDisable()
 	{
+		if(Leveling.i!= null) Leveling.i.GainExp(exp);
 		if(EnemiesManager.i != null) EnemiesManager.i.enemies.Remove(transform);
 	}
 }
