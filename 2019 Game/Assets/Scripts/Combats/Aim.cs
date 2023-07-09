3160b630c88af0708e490de63190f2926d3e1328
Huntrt <huntrt.business@outlook.com>
2023-07-09 14:24:26 +0700

    Ablew to have custom body to aim

--- 2019 Game/Assets/Scripts/Combats/Aim.cs
+++ 2019 Game/Assets/Scripts/Combats/Aim.cs
@@ -2,6 +2,7 @@
 
 public class Aim : MonoBehaviour
 {
+	[SerializeField] Transform body;
 	[SerializeField] Transform[] firepoints;
 	[SerializeField] Caster caster;
 	[SerializeField] float detect;
@@ -9,24 +10,30 @@ public class Aim : MonoBehaviour
 	public Vector2 dirTotarget {get {return target.position - transform.position;}}
 	bool aimingRight = true;
 
+	void Awake()
+	{
+		//Auto get body if havent
+		if(body == null) body = transform;
+	}
+
 	void Update()
 	{
-		//Stop if there no target
-		if(target == null) return;
+		//Stop if there no target or no longer active
+		if(target == null && !target.gameObject.activeInHierarchy) return;
 		//Make all firepoint aim toward target direction
 		foreach (var firepoint in firepoints) firepoint.right = dirTotarget;
 		//Begin auto cast when the target get in detection
-		caster.autoCast = Vector2.Distance(transform.position, target.position) <= detect;
+		caster.autoCast = Vector2.Distance(body.position, target.position) <= detect;
 		//@ Flip the rotation if the target is behind/infront
 		if(dirTotarget.x > 0 && !aimingRight)
 		{
 			aimingRight = true;
-			transform.rotation = Quaternion.Euler(transform.localEulerAngles.x, 0, transform.localEulerAngles.z);
+			body.rotation = Quaternion.Euler(body.localEulerAngles.x, 0, body.localEulerAngles.z);
 		}
 		if(dirTotarget.x < 0 && aimingRight)
 		{
 			aimingRight = false;
-			transform.rotation = Quaternion.Euler(transform.localEulerAngles.x, 180, transform.localEulerAngles.z);
+			body.rotation = Quaternion.Euler(body.localEulerAngles.x, 180, body.localEulerAngles.z);
 		}
 	}
 }
