ac599d9f5cb7684a893bf3cc854989aa00e05694
Huntrt <huntrt.business@outlook.com>
2023-07-09 15:34:05 +0700

    Completed annoyed state

--- 2019 Game/Assets/Scripts/Protagonist/ProtagState.cs
+++ 2019 Game/Assets/Scripts/Protagonist/ProtagState.cs
@@ -6,7 +6,7 @@ public class ProtagState : MonoBehaviour
 	{
 		Idle,// do nothing mostly switch to stomp instantly.
 		Melee, //attack close up enemy with crowd control weapon.
-		Range, //attack enemy from far away if got got hurt by it.
+		Annoyed, //attack enemy from far away if got got hurt by it.
 		Stomping, // when no threat begin stomp he floor.
 		Reviving, // 10 second of unable to do anything.
 	}
@@ -14,9 +14,27 @@ public class ProtagState : MonoBehaviour
 	[SerializeField] float meleeRange;
 	[SerializeField] float revivingDuration;
 	[SerializeField] LayerMask enemyLayer;
+	[SerializeField] Health protagHealth;
+	[SerializeField] ProtagEquip equip;
+
+	void OnEnable()
+	{
+		protagHealth.OnDamage += GotAnnoyed;
+	}
+
+	void OnDisable()
+	{
+		protagHealth.OnDamage -= GotAnnoyed;
+	}
 
 	void Update()
 	{
+		//If there is target but it is inactive
+		if(equip.currentTarget != null) if(!equip.currentTarget.gameObject.activeInHierarchy)
+		{
+			//No longer have target
+			equip.currentTarget = null;
+		}
 		InMeleeRange();
 	}
 
@@ -27,16 +45,31 @@ public class ProtagState : MonoBehaviour
 		//If there enemy in melee range
 		if(scan)
 		{
-			//If this is the initial stateor the target no longer active
-			if(currentState != State.Melee || !Protag.i.equip.currentTarget.gameObject.activeInHierarchy)
+			//If initial melee or have no target
+			if(currentState != State.Melee || equip.currentTarget == null)
 			{
 				//Set current target to be enemy got scan
-				Protag.i.equip.currentTarget = scan.collider.gameObject.transform;
+				equip.currentTarget = scan.collider.gameObject.transform;
 			}
 			//Change stage
 			currentState = State.Melee;
 			//Switch to an rapid firing weapon
-			Protag.i.equip.SwitchWeaponType(ProtagWeapon.Type.rapid);
+			equip.SwitchWeaponType(ProtagWeapon.Type.rapid);
 		}
+	}
+
+	void GotAnnoyed(float taken)
+	{
+		//Dont get annoy if currently melee
+		if(currentState == State.Melee) return;
+		if(taken <= 0) return;
+		//Protag got annoyed
+		currentState = State.Annoyed;
+		//Switch to an sniping firing weapon
+		equip.SwitchWeaponType(ProtagWeapon.Type.sniping);
+		//Get the list of enemy
+		var enemies = EnemiesManager.i.enemies;
+		//Randomly an enemy in the list
+		equip.currentTarget = enemies[Random.Range(0, enemies.Count)].transform;
 	}
 }
