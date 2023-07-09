c9df77f4a89ccf04495a40e2c56ae30086fae989
Huntrt <huntrt.business@outlook.com>
2023-07-09 16:56:49 +0700

    Display the protag health

--- 2019 Game/Assets/Scripts/Protagonist/Protag.cs
+++ 2019 Game/Assets/Scripts/Protagonist/Protag.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using UnityEngine.UI;
+using UnityEngine;
 
 public class Protag : MonoBehaviour
 {
@@ -19,4 +20,27 @@ public class Protag : MonoBehaviour
 	public ProtagEquip equip;
 	public ProtagState state;
 	public Health health;
+	[SerializeField] Image healthBar;
+	[SerializeField] TMPro.TextMeshProUGUI healthCount;
+
+	void OnEnable()
+	{
+		health.OnDamage += RefreshHealthGUI;
+	}
+
+	void OnDisable()
+	{
+		health.OnDamage -= RefreshHealthGUI;
+	}
+
+	void Start()
+	{
+		RefreshHealthGUI(0);
+	}
+
+	void RefreshHealthGUI(float taken)
+	{
+		healthBar.fillAmount = health.CurHealth/health.MaxHealth;
+		healthCount.text = health.CurHealth.ToString();
+	}
 }
